bf41c335c58c38de2d0124f2abb1b6f7369f2d23
Phillip Marques <phillipmarq@hotmail.com>
2022-01-10 17:10:38 -0300

    Ajuste de Cliente

--- src/main/java/com/pms/msrlog/model/Cliente.java
+++ src/main/java/com/pms/msrlog/model/Cliente.java
@@ -1,6 +1,10 @@
 package com.pms.msrlog.model;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Cliente {
 	
 	private Long id;
@@ -8,32 +12,5 @@ public class Cliente {
 	private String email;
 	private String telefone;
 	
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getTelefone() {
-		return telefone;
-	}
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
-	}
-	
-	
-	
+
 }
