c098b084458c5cb697e475788669b18d06882686
Phillip Marques <phillipmarq@hotmail.com>
2022-01-12 16:03:10 -0300

    desenvolvendo api

--- src/main/java/com/pms/msrlog/controller/ClienteController.java
+++ src/main/java/com/pms/msrlog/controller/ClienteController.java
@@ -1,31 +1,28 @@
 package com.pms.msrlog.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pms.msrlog.model.Cliente;
+import com.pms.msrlog.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 	
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar(){
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Phillip");
-		cliente1.setTelefone("11 99999-1234");
-		cliente1.setEmail("phillip@phillip.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Alceu");
-		cliente2.setTelefone("11 99999-1235");
-		cliente2.setEmail("alceu@alceu.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 
 }
